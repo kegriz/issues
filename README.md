2deeb5aac9cfe96d0b1ee8ed961d594f6037e918
kegriz <rafalrudko.cmzzs@gmail.com>
2018-02-20 18:57:52 +0100

    clickable buttons

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # issues
 App for fetching GitHub issues
+
+<a target="new" href="http://rafalrudko.com/code/issues/">WEBSITE</a>
+
+clone repo with GIT:
+$ git clone https://github.com/kegriz/issues.git
+
+instal packages:
+$ npm install
+
+start app:
+$ npm start
--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import $ from 'jquery';
 
 $( () => {
 
-  // document.getElementById("iid").innerHTML = "Hello Issues";
+  // stars toogle highlight
+  $(".star").click(function() {
+    $(this).toggleClass("star--checked");
+  });
+
+  // choosing filters
+  $(".sidebar-content__element").click(function() {
+    $(".sidebar-content__element").removeClass("selected");
+    $(this).addClass("selected");
+  });
+  
+  // close button
+  $(".JS-exit").click(function() {
+    $(".app-container").fadeOut("slow");
+  });
 
 });
